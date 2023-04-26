d0e8d0252ee4aecf65336e5fbde3072cad0a8978
brysonfenn <bryson.fenn1@gmail.com>
2023-04-26 17:02:10 -0600

    Separated pokemon from pokemon list

--- attacks_list.c
+++ attacks_list.c
@@ -3,17 +3,17 @@
 #define NUM_ATTACKS 50
 
 attack empty_attack=  {"------------",  0,  0, 100, 0, 0, 0, 0 };
-//				 	           "Name        "  id  pp  pwr  acc   type    ad dd sd acd  pmove
-attack tackle       = {"Tackle      ",  1, 35,  40,  95, NORMAL,   0, 0, 0, 0.0, false, &attack_do_nothing, 0, 0 };
-attack scratch      = {"Scratch     ",  2, 35,  40, 100, NORMAL,   0, 0, 0, 0.0, false, &attack_do_nothing, 0, 0 };
-attack growl        = {"Growl       ",  3, 40,   0, 100, NO_TYPE,  1, 0, 0, 0.0, false, &attack_do_nothing, 0, 0 };
-attack tail_whip    = {"Tail Whip   ",  4, 30,   0, 100, NO_TYPE,  0, 1, 0, 0.0, false, &attack_do_nothing, 0, 0 };
-attack string_shot  = {"String Shot ",  5, 40,   0,  95, NO_TYPE,  0, 0, 1, 0.0, false, &attack_do_nothing, 0, 0 };
+//				 	           "Name        "  id  pp  pwr  acc   type     ad dd sd acd  priority    effect         params
+attack tackle       = {"Tackle      ",  1, 35,  40,  95, NORMAL,   0, 0, 0, 0.0, false, &attack_do_nothing };
+attack scratch      = {"Scratch     ",  2, 35,  40, 100, NORMAL,   0, 0, 0, 0.0, false, &attack_do_nothing };
+attack growl        = {"Growl       ",  3, 40,   0, 100, NO_TYPE,  1, 0, 0, 0.0, false, &attack_do_nothing };
+attack tail_whip    = {"Tail Whip   ",  4, 30,   0, 100, NO_TYPE,  0, 1, 0, 0.0, false, &attack_do_nothing };
+attack string_shot  = {"String Shot ",  5, 40,   0,  95, NO_TYPE,  0, 0, 1, 0.0, false, &attack_do_nothing };
 attack poison_sting = {"Poison Sting",  6, 35,  15, 100, POISON,   0, 0, 0, 0.0, false, &inflict_condition, POISONED, 30 };
-attack sand_attack  = {"Sand Attack ",  7, 15,   0, 100, NO_TYPE,  0, 0, 0, 0.1, false, &attack_do_nothing, 0, 0 };
-attack quick_attack = {"Quick Attack",  8, 30,  40, 100, NORMAL,   0, 0, 0, 0.0,  true, &attack_do_nothing, 0, 0 };
-attack defense_curl = {"Defense Curl",  9, 40,   0, 100, NO_TYPE,  0,-1, 0, 0.0, false, &attack_do_nothing, 0, 0 };
-attack vine_whip    = {"Vine whip   ", 10, 25,  45, 100, GRASS,    0, 0, 0, 0.0, false, &attack_do_nothing, 0, 0 };
+attack sand_attack  = {"Sand Attack ",  7, 15,   0, 100, NO_TYPE,  0, 0, 0, 0.1, false, &attack_do_nothing };
+attack quick_attack = {"Quick Attack",  8, 30,  40, 100, NORMAL,   0, 0, 0, 0.0,  true, &attack_do_nothing };
+attack defense_curl = {"Defense Curl",  9, 40,   0, 100, NO_TYPE,  0,-1, 0, 0.0, false, &attack_do_nothing };
+attack vine_whip    = {"Vine whip   ", 10, 25,  45, 100, GRASS,    0, 0, 0, 0.0, false, &attack_do_nothing };
 
 static attack * local_array[NUM_ATTACKS] = { &empty_attack, 
     &tackle, &scratch, &growl, &tail_whip, &string_shot, &poison_sting, &sand_attack, &quick_attack, &defense_curl, &vine_whip }; // #1-10
--- pokemon.c
+++ pokemon.c
@@ -8,24 +8,6 @@
 
 #define LINE_SIZE 100
 
-// pok  varName   = {"name here",  id  hp chp   ba   bd    bs   type1     type2    num_att ca cd cs  acc lvl exp}
-pokemon emptyPok = {"MissingNo",    0,100,100,   0,   0,    0, NO_TYPE,  NO_TYPE };
-pokemon bulbasaur = {"Bulbasaur",   1,  9,  9,   4,   4,    4, GRASS,    POISON };
-pokemon charmander = {"Charmander", 4,  9,  9,   4,   4,    4, FIRE,     NO_TYPE };
-pokemon squirtle = {"Squirtle",     7,  9,  9,   4,   4,    4, WATER,    NO_TYPE };
-pokemon caterpie = {"Caterpie",    10,  6,  6,   2,   2,    3, BUG,      NO_TYPE };
-pokemon weedle   = {"Weedle",      13,  7,  7,   3,   2,    3, BUG,      POISON };
-pokemon pidgey   = {"Pidgey",      16,  8,  8,   4,   2,    3, FLYING,   NORMAL };
-pokemon rattata  = {"Rattata",     19,  7,  7,   2,   3,    4, NORMAL,   NO_TYPE };
-pokemon sandshrew= {"Sandshrew",   27,  7,  7,   3,   4,    2, GROUND,   NO_TYPE };
-
-#define NUM_CREATED_POKEMON 8
-#define NUM_STARTERS 3
-#define NUM_WILD_POKEMON (NUM_CREATED_POKEMON - NUM_STARTERS)
-static pokemon * pokList[10] = { &bulbasaur, &charmander, &squirtle, &caterpie, &weedle, &pidgey, &rattata, &sandshrew };
-
-static pokemon newest_pokemon;
-
 void randomize_stats(pokemon * new_pok, int level, int level_min, int level_max);
 
 void pokemon_init(pokemon * new_pok, int level, int level_min, int level_max) {
@@ -33,22 +15,6 @@ void pokemon_init(pokemon * new_pok, int level, int level_min, int level_max) {
   new_pok->condition = NO_CONDITION;
   new_pok->numAttacks = 0;
   pokemon_give_moves(new_pok);
-}
-
-pokemon * get_random_pokemon(int level_min, int level_max) {
-  newest_pokemon = *(pokList[rand() %  NUM_CREATED_POKEMON]);
-  pokemon * new_pok = &newest_pokemon;
-  pokemon_init(new_pok, RANDOM_LEVEL, level_min, level_max);
-  return new_pok;
-}
-
-//Always immediately dereference the return value of this function.
-pokemon * get_random_wild_pokemon(int level_min, int level_max) {
-  newest_pokemon = *(pokList[(rand() % NUM_WILD_POKEMON) + NUM_STARTERS]);
-  // newest_pokemon = bulbasaur;
-  pokemon * new_pok = &newest_pokemon;
-  pokemon_init(new_pok, RANDOM_LEVEL, level_min, level_max);
-  return new_pok;
 }
 
 void randomize_stats(pokemon * new_pok, int level, int level_min, int level_max) {
