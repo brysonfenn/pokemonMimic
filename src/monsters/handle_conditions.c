1db4ffc5adcf5fa7b3086ef465694d0641140bed
brysonfenn <bryson.fenn1@gmail.com>
2023-06-15 13:49:02 -0600

    Added end conditions c file

---
+++ src/monsters/handle_conditions.c
@@ -0,0 +1,100 @@
+#include "conditions.h"
+
+#include <stdlib.h>
+
+#include "pokemon.h"
+#include "../player.h"
+#include "../print/print_utils.h"
+#include "../print/print_defines.h"
+#include "../print/print_battle.h"
+
+int handle_leech_seed(bool enemy, struct Pokemon * pok, struct Pokemon * perp);
+int handle_hurt_1_16(bool enemy, struct Pokemon * pok, char * pok_condition_text);
+
+//Handle poisoning, leech seed, etc.
+int handle_end_conditions(struct Pokemon * pok) {
+    bool enemy;
+    Pokemon * perp;
+    bool success = false;
+
+    //Set enemy boolean and perp
+    if (pok == player.enemy_pokemon) {
+        enemy = true;
+        perp = player.current_pokemon;
+    }
+    else {
+        enemy = false;
+        perp = player.enemy_pokemon;
+    }
+
+	//If either pokemon fainted, do not handle conditions
+	if (pok->currentHP <= 0 || perp->currentHP <= 0) {
+		return 0;
+	}
+
+	//Remove flinched hidden condition
+	remove_hidden_condition(pok, FLINCHED);
+
+    text_box_cursors(TEXT_BOX_BEGINNING);
+
+	//Poison and burn
+	if (pok->visible_condition == POISONED) {
+		handle_hurt_1_16(enemy, pok, "poison");
+        if (pok->currentHP == 0) return 2;
+	}
+    if (pok->visible_condition == BURNED) {
+        handle_hurt_1_16(enemy, pok, "burn");
+        if (pok->currentHP == 0) return 2;
+    }
+    if (has_hidden_condition(pok, BIND)) {
+        success = decrement_hidden_condition_val(pok, BIND);
+        if (!success) {
+            printw("%s was released from bind", pok->name); refresh(); sleep(2);
+            return 3;
+        }
+
+        handle_hurt_1_16(enemy, pok, "bind");
+        if (pok->currentHP == 0) return 2;
+    }
+	
+	//Leech Seed
+	if (has_hidden_condition(pok, SEEDED)) {
+        handle_leech_seed(enemy, pok, perp);
+        if (pok->currentHP == 0) return 2;
+	}
+
+	return 0;
+}
+
+
+int handle_leech_seed(bool enemy, struct Pokemon * pok, struct Pokemon * perp) {
+    clear(); printBattle();
+
+    text_box_cursors(TEXT_BOX_BEGINNING);
+    if (enemy) printw("%s", ENEMY_TEXT);
+    printw("%s's HP was sapped!", pok->name); refresh(); sleep(1);
+    int sappedHP = ((pok->maxHP / 8) + 1);
+
+    blinkPokemon(false, DAMAGED_COLOR, LEECH_SEED_BLINK_TIMES); 
+    printBattle();
+    blinkPokemon(true, HEAL_COLOR, LEECH_SEED_BLINK_TIMES);
+
+    //Give HP equal to taken HP and fix any issues
+    if (sappedHP > pok->currentHP) { perp->currentHP += pok->currentHP; pok->currentHP = 0; }
+    else { perp->currentHP += sappedHP; pok->currentHP -= sappedHP; }
+    if (perp->currentHP > perp->maxHP) perp->currentHP = perp->maxHP;
+
+    return 0;
+}
+
+
+int handle_hurt_1_16(bool enemy, struct Pokemon * pok, char * pok_condition_text) {
+    clear(); printBattle();
+
+    text_box_cursors(TEXT_BOX_BEGINNING);
+    if (enemy) printw("%s", ENEMY_TEXT);
+    printw("%s was hurt by %s!", pok->name, pok_condition_text);
+    pok->currentHP -= ((pok->maxHP / 16) + 1);
+    if (pok->currentHP < 0) pok->currentHP = 0;
+    refresh(); sleep(2);
+}
